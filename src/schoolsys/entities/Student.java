c008c54deb2496e07108c9b7946c360297faa0bb
rodrigovcf <rodrigo.rvcf@gmail.com>
2022-03-09 14:39:23 -0300

    config project with lombok

--- src/schoolsys/entities/Student.java
+++ src/schoolsys/entities/Student.java
@@ -1,89 +1,21 @@
 package schoolsys.entities;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Student{
 
 	private int id;
+	
 	private String name;
 	private String schoolClass;
 	private double sc1, sc2, sc3, score;
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getSchoolClass() {
-		return schoolClass;
-	}
-	public void setSchoolClass(String schoolClass) {
-		this.schoolClass = schoolClass;
-	}
 	
-	public double getSc1() {
-		return sc1;
-	}
-	public void setSc1(double sc1) {
-		this.sc1 = sc1;
-	}
-	public double getSc2() {
-		return sc2;
-	}
-	public void setSc2(double sc2) {
-		this.sc2 = sc2;
-	}
-	public double getSc3() {
-		return sc3;
-	}
-	public void setSc3(double sc3) {
-		this.sc3 = sc3;
-	}
-	public double getAverage() {
-		return score;
-	}
-	public void setAverage(double average) {
-		this.score = average;
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((schoolClass == null) ? 0 : schoolClass.hashCode());
-		return result;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Student other = (Student) obj;
-		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (schoolClass == null) {
-			if (other.schoolClass != null)
-				return false;
-		} else if (!schoolClass.equals(other.schoolClass))
-			return false;
-		return true;
-	}
 		
 	
 }
--- src/schoolsys/entities/Teacher.java
+++ src/schoolsys/entities/Teacher.java
@@ -1,5 +1,12 @@
 package schoolsys.entities;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Teacher {
 
 	private int id;
@@ -13,56 +20,4 @@ public class Teacher {
 		this.component = component;
 	}
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public CurricularComponent getComponent() {
-		return component;
-	}
-	public void setComponent(CurricularComponent component) {
-		this.component = component;
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((component == null) ? 0 : component.hashCode());
-		result = prime * result + id;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Teacher other = (Teacher) obj;
-		if (component == null) {
-			if (other.component != null)
-				return false;
-		} else if (!component.equals(other.component))
-			return false;
-		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
-	}	
-
 }
